880a0aa4ca2197fa8ac0c715ccf5545f33d5c224
Ryan Hewitt <ryan@mesuva.com.au>
2016-11-24 19:51:16 +1030

    Addition of random order options for product list block

--- controller.php
+++ controller.php
@@ -16,7 +16,7 @@ class Controller extends Package
 {
     protected $pkgHandle = 'community_store';
     protected $appVersionRequired = '5.7.5';
-    protected $pkgVersion = '0.9.9.6';
+    protected $pkgVersion = '0.9.9.7';
 
     public function getPackageDescription()
     {
