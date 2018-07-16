caff85f11c5f00b29dd252da931dde559ebdedfe
Ryan Hewitt <ryan@mesuva.com.au>
2018-07-16 09:54:39 +0930

    Completion of Product group editing in dashboard

--- controllers/single_page/dashboard/store/products/groups.php
+++ controllers/single_page/dashboard/store/products/groups.php
@@ -5,6 +5,7 @@ namespace Concrete\Package\CommunityStore\Controller\SinglePage\Dashboard\Store\
 use \Concrete\Core\Page\Controller\DashboardPageController;
 use \Concrete\Package\CommunityStore\Src\CommunityStore\Group\Group as StoreGroup;
 use \Concrete\Package\CommunityStore\Src\CommunityStore\Group\GroupList as StoreGroupList;
+use \Concrete\Package\CommunityStore\Src\CommunityStore\Product\ProductGroup as StoreProductGroup;
 
 
 class Groups extends DashboardPageController
@@ -17,25 +18,70 @@ class Groups extends DashboardPageController
         $this->requireAsset('css', 'communityStoreDashboard');
         $this->requireAsset('javascript', 'communityStoreFunctions');
     }
-    public function groupadded()
+
+    public function add()
     {
-        $this->set('success',"Product Group Created");
-        $this->view();
-    }
-    public function addgroup()
-    {
-        $this->view();
-        $this->error = null; //clear errors
-        $errors = $this->validateGroup($this->post());
-        $this->error = $errors;
-        if (!$errors->has()) {
-            StoreGroup::add($this->post('groupName'));
-            $this->redirect('/dashboard/store/products/groups/', 'groupadded');
+        $this->requireAsset('css', 'communityStoreDashboard');
+        $this->set('pageTitle', t('Add Product Group'));
+        $this->set('group', new StoreGroup());
+        $this->requireAsset('css', 'select2');
+        $this->requireAsset('javascript', 'select2');
+
+        if ($this->post()) {
+            $errors = $this->validateGroup($this->post());
+            $this->error = $errors;
+            if (!$errors->has()) {
+                $newproductgroup = StoreGroup::add($this->post('groupName'));
+
+                $productids = $this->post('products');
+
+                if (is_array($productids)) {
+                    $productids = array_unique($productids);
+
+                    foreach($productids as $productid) {
+                        StoreProductGroup::add($productid, $newproductgroup->getID());
+                    }
+                }
+
+                $this->flash('success', t('Product Group Added'));
+                $this->redirect('/dashboard/store/products/groups/');
+            }
         }
     }
-    public function editgroup($gID)
+    public function edit($gID)
     {
-        StoreGroup::getByID($gID)->update($this->post('gName'));
+        $this->requireAsset('css', 'communityStoreDashboard');
+        $this->set('pageTitle', t('Edit Product Group'));
+        $this->requireAsset('css', 'select2');
+        $this->requireAsset('javascript', 'select2');
+
+        $group = StoreGroup::getByID($gID);
+
+        if ($this->post()) {
+            $this->error = null; //clear errors
+            $errors = $this->validateGroup($this->post());
+            $this->error = $errors;
+            if (!$errors->has()) {
+                $group->update($this->post('groupName'));
+
+                StoreProductGroup::removeProductsForGroup($group);
+
+                $productids = $this->post('products');
+
+                if (is_array($productids)) {
+                    $productids = array_unique($productids);
+
+                    foreach($productids as $productid) {
+                        StoreProductGroup::add($productid, $group->getID());
+                    }
+                }
+
+                $this->flash('success', t('Product Group Edited'));
+                $this->redirect('/dashboard/store/products/groups/');
+            }
+        }
+
+        $this->set('group', $group);
     }
     public function validateGroup($args)
     {
