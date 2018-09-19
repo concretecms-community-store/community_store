d7fb5706e704d98942a7a7f17512829675261ef3
Ryan Hewitt <ryan@mesuva.com.au>
2018-09-19 20:19:14 +0930

    Increment package number

--- controller.php
+++ controller.php
@@ -16,7 +16,7 @@ class Controller extends Package
 {
     protected $pkgHandle = 'community_store';
     protected $appVersionRequired = '5.7.5';
-    protected $pkgVersion = '1.4.1.3';
+    protected $pkgVersion = '1.4.2';
 
     public function getPackageDescription()
     {
