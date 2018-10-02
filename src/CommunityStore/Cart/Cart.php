d03bf252e91d2ada15cd6fe9336800535d9ff64b
Ryan Hewitt <ryan@mesuva.com.au>
2018-10-02 15:05:57 +0930

    Fix to updating product quantity on cart page when cart quantity is fixed to 1

--- src/CommunityStore/Cart/Cart.php
+++ src/CommunityStore/Cart/Cart.php
@@ -390,7 +390,7 @@ class Cart
 
         $product = StoreProduct::getByID((int) $cart[$instanceID]['product']['pID']);
 
-        if (!$product->allowDecimalQuantity()) {
+        if ($product && !$product->allowDecimalQuantity()) {
             $qty = (int) $data['pQty'];
         }
 
