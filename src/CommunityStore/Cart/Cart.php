5f82768183b4f88f21eebc50bda827b6d7e2022b
Ryan Hewitt <ryan@mesuva.com.au>
2018-11-19 10:41:33 +0000

    Wrong spot

--- src/CommunityStore/Cart/Cart.php
+++ src/CommunityStore/Cart/Cart.php
@@ -195,10 +195,11 @@ class Cart
             unset($data['pID']);
             unset($data['quantity']);
             unset($data['customerPrice']);
+            unset($data['ccm_token']);
 
             //since we removed the ID/qty, we're left with just the attributes
             $cartItem['productAttributes'] = $data;
-            unset($data['productAttributes']['ccm_token']);
+
 
             $removeexistingexclusive = false;
 
