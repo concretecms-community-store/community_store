072e9b83a01c371d91437c8a97652916d905026f
Ryan Hewitt <ryan@mesuva.com.au>
2018-11-21 15:03:48 +0000

    Change delete order button to form to allow for CSRF token

--- controllers/single_page/dashboard/store/orders.php
+++ controllers/single_page/dashboard/store/orders.php
@@ -208,7 +208,8 @@ class Orders extends DashboardPageController
         if ($this->token->validate('community_store')) {
             StoreOrder::getByID($oID)->remove();
             $this->flash('success', t('Order Deleted'));
-            return \Redirect::to('/dashboard/store/orders');
         }
+
+        return \Redirect::to('/dashboard/store/orders');
     }
 }
