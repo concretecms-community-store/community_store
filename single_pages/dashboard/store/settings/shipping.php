cfcd087f777cdbab61fb9c81064f4098761895da
Ryan Hewitt <ryan@mesuva.com.au>
2022-06-13 17:26:49 +0100

    Fix typo, correct behaviour of shipping option config forms

--- single_pages/dashboard/store/settings/shipping.php
+++ single_pages/dashboard/store/settings/shipping.php
@@ -97,7 +97,7 @@ if (in_array($controller->getAction(), $addViews)) {
 
                 </div>
                 <hr>
-                <?php isset($sm) ? $smt->renderDashboardForm($sm) : false; ?>
+                <?php isset($smt) ? $smt->renderDashboardForm($sm) : false; ?>
             </div>
         </div>
 
