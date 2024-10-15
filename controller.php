2733c10d2f1fed9d5398293a2662b4dbddcbd985
Ryan Hewitt <ryan@mesuva.co.uk>
2024-10-15 18:39:54 +0100

    Update version for release

--- controller.php
+++ controller.php
@@ -27,7 +27,7 @@ class Controller extends Package implements ProviderAggregateInterface
 {
     protected $pkgHandle = 'community_store';
     protected $appVersionRequired = '8.5';
-    protected $pkgVersion = '2.7.2-alpha2';
+    protected $pkgVersion = '2.7.2';
 
     protected $npmPackages = [
         'sysend' => '1.3.4',
